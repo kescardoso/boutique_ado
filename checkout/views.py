a15d47936e28d1cb2f0e19b938212a2a65db4069
Kes Cardoso <kesia.development@gmail.com>
2020-06-17 15:47:32 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Gv1ugDox3glXEAE376A7dbWYqDVEPkLv1Puh0RbyIFbfNanhsW4vKePyxWhUUbXUAvoJLPA9rqfxrr1uazjr8YG00JSaxgORr',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
