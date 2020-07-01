c6b9352adce990122252bf5c4672d5f620f7f0c4
Kes Cardoso <kesia.development@gmail.com>
2020-07-01 20:45:38 +0000

    Deploy the heroku app with guinicorn and create a Procfile

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,7 +22,8 @@ SECRET_KEY = 'bgi8^oi=4zjges3ls@m6)=rh^@9@v8qg^*8&_mapw@55^mi3q!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost',
+                'kika-boutique-ado.herokuapp.com']
 
 # Application definition
 
@@ -115,12 +116,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASE_URL = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
