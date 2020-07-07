744e4aaef0cb14136655506282e0c05cca7eb63d
Kes Cardoso <kesia.development@gmail.com>
2020-07-07 20:05:22 +0000

    Python refactoring and deployed static and media files to AWS3 for back and front end

--- profiles/forms.py
+++ profiles/forms.py
@@ -5,7 +5,7 @@ from .models import UserProfile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        exclude = ('user',)
+        exclude = ('user', )
 
     def __init__(self, *args, **kwargs):
         """
@@ -24,11 +24,12 @@ class UserProfileForm(forms.ModelForm):
 
         self.fields['default_phone_number'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if field != 'default_country': 
+            if field != 'default_country':
                 if self.fields[field].required:
                     placeholder = f'{placeholders[field]} *'
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-                self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+                self.fields[field].widget.attrs[
+                    'class'] = 'border-black rounded-0 profile-form-input'
                 self.fields[field].label = False
