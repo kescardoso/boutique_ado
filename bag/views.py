8a43928bb82e09d2316d15c0dcce4d833f39b4ab
Kes Cardoso <kesia.development@gmail.com>
2020-07-17 14:46:50 +0000

    Troubleshooting the bag app from apps, contexts, urls and views

--- bag/views.py
+++ bag/views.py
@@ -91,7 +91,7 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """ Remove the tiem from the shopping bag """
+    """ Remove the item from the shopping bag """
 
     try:
         product = get_object_or_404(Product, pk=item_id)
